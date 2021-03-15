4eb486f496525bfb26949997ecd5cb5ea652b10c
Riho <Rix1113@gmail.com>
2021-03-15 19:22:46 +0200

    Add code

---
+++ src/JavaFundamentalsLiveCoding/Task7/Main.java
@@ -0,0 +1,23 @@
+package JavaFundamentalsLiveCoding.Task7;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Which fibonacci number do you want: ");
+        int input = scanner.nextInt();
+
+        int count = 1;
+        int a = 0, b = 1, sum = 0;
+        while (count <= input) {
+            if (count == input) {
+                System.out.println(input + " fibonacci number is " +a);
+            }
+            sum = a + b;
+            a = b;
+            b = sum;
+            count++;
+        }
+    }
+}
