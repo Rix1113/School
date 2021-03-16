e5a5a0a8305d6465ed42a5be204cddf76066fd83
Rix <80359407+Rix1113@users.noreply.github.com>
2021-03-16 14:15:41 +0200

    Change ht to th

--- src/JavaFundamentalsLiveCoding/Task9/Main.java
+++ src/JavaFundamentalsLiveCoding/Task9/Main.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        /* Wave height */
-        int wave_height = 4;
+        /* Wave heigth */
+        int wave_heigth = 4;
 
-        /* Scanner input wave lenght */
+        /* Scanner input wave length */
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter wave lenght: ");
+        System.out.print("Enter wave length: ");
         int wave_length = scanner.nextInt();
 
-        pattern(wave_height, wave_length);
+        pattern(wave_heigth, wave_length);
     }
 
-    static void pattern(int wave_height,
+    static void pattern(int wave_heigth,
                         int wave_length)
     {
         int i, j, k, e, n, count, x;
@@ -24,16 +24,16 @@ public class Main {
         x = 1;
 
         /* Wave height */
-        for (i = 0; i < wave_height; i++)
+        for (i = 0; i < wave_heigth; i++)
         {
-            for (j = wave_height; j <= wave_height + i; j++)
+            for (j = wave_heigth; j <= wave_heigth + i; j++)
                 System.out.print(" ");
 
-            /* Wave lenght */
+            /* Wave length */
             for (count = 1; count <= wave_length; count++)
             {
                /* Spaces */
-                for (n = (wave_height + wave_height - 2); n >= x; n--)
+                for (n = (wave_heigth + wave_heigth - 2); n >= x; n--)
                     System.out.print(" ");
 
                 for (k = 1; k <= e; k++)
